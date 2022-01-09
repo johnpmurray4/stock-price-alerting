8c54547f76f5af2307a1d3ee127fa5fbb97eec9f
John Murray <johnpmurray4@gmail.com>
2022-01-09 17:43:41 +0000

    Added email func

--- price_alert.py
+++ price_alert.py
@@ -1,6 +1,8 @@
 import os.path
 import yfinance as yf
 import pandas as pd
+import smtplib
+import configparser
 
 
 def main():
@@ -13,19 +15,19 @@ def main():
 def check_prices(tickersToCheck):
     for line in tickersToCheck:
         ticker, stopLoss, target = line.split(",", 3)
-        print("Checking price for " +
-              ticker + ", stop loss is " + stopLoss + ", with target " + target)
+        print("Checking price for %s, stop loss is %s, with target %s" %
+              (ticker, stopLoss, target))
 
         price = get_price(ticker)
-        print("Price for ticker " + ticker + " is " + str(price))
+        print("Price for ticker %s is %s" % (ticker, str(price)))
 
         if (price < float(stopLoss)):
             print("Price is lower than stop loss")
-            send_email("STOP", price, float(stopLoss))
+            send_email("Stop loss hit", ticker, price, float(stopLoss))
 
         if (price > float(target)):
             print("Price is higher than target")
-            send_email("TARGET_MET", price, float(target))
+            send_email("Target met", ticker, price, float(target))
 
 
 def get_price(ticker: str) -> float:
@@ -34,8 +36,28 @@ def get_price(ticker: str) -> float:
     return float(data.tail(1).iloc[0]['Close'])
 
 
-def send_email(action: str, currentPrice: float, targetPrice: float):
+def send_email(action: str, ticker: str, currentPrice: float, targetPrice: float):
     print("Sending email alert")
+    config = configparser.RawConfigParser()
+    config.read('properties.config')
+
+    gmail_user = config.get('EMAIL_CREDENTIALS', 'username')
+    gmail_password = config.get('EMAIL_CREDENTIALS', 'password')
+
+    sent_from = gmail_user
+    to = [config.get('EMAIL_RECIPIENT', 'recipient')]
+    subject = action
+    body = "Current price for ticker %s is %s - alert at %s" % (
+        ticker, str(currentPrice), str(targetPrice))
+
+    message = """From: %s\nTo: %s\nSubject: %s\n\n%s
+    """ % (sent_from, ", ".join(to), subject, body)
+
+    server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
+    server.ehlo()
+    server.login(gmail_user, gmail_password)
+    server.sendmail(sent_from, to, message)
+    server.close()
 
 
 if __name__ == "__main__":
